03e1fd0693f53e47518301df4fcc73edf527d8c0
Xen <twidlestick@gmail.com>
2024-02-08 19:59:46 +0530

    minor changes

--- core/Colors.h
+++ core/Colors.h
@@ -1,6 +1,8 @@
 #pragma once
 
 namespace goof {
+
+	//need to delete this wonky as fk thng 
 		static float RED[4] = { 1.0f , 0.0f, 0.0f,1.0f };
 		static float BLUE[4] = { 0.0f , 0.0f, 1.0f,1.0f };
 		static float GREEN[4] = { 0.0f , 1.0f, 0.0f,1.0f };
